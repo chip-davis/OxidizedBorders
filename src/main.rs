73a15da035e1e8c5d579dcc3dfa6e6c570381858
chip-davis <chipydavis99@gmail.com>
2026-02-08 15:05:37 -0600

    added basic permission check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use objc2_application_services::AXIsProcessTrusted;
+use objc2_core_graphics::{CGPreflightScreenCaptureAccess, CGRequestScreenCaptureAccess};
+
+fn check_permissions() {
+    unsafe {
+        if !CGPreflightScreenCaptureAccess() {
+            if !CGRequestScreenCaptureAccess() {
+                println!("failed to access accessability");
+                std::process::exit(1);
+            }
+        }
+        if !AXIsProcessTrusted() {
+            println!("not trusted");
+            std::process::exit(1);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    check_permissions();
 }
